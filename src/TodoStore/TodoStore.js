3e0c0d22ebe8307419edfd54e64514065e3664bc
syedgufran1997 <syedgufran1997@gmail.com>
2021-02-12 16:35:12 +0530

    Solved deploy bugs

--- src/TodoStore/TodoStore.js
+++ src/TodoStore/TodoStore.js
@@ -1,8 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import { AdminReducer } from "../TodoReducer/AdminReducer";
 import { TodoReducer } from "../TodoReducer/TodoReducer";
-import { UserReducer } from "../TodoReducer/UserReducer";
 
 const rootReducer = combineReducers({
   todoStore: TodoReducer,
